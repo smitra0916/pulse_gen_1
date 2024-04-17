360a115a34a324b71b04e1e5ac87bab856dd3de9
smitra0916 <mitrasourasish@gmail.com>
2024-04-18 03:08:09 +0530

    Update pulse_gen_1.xpr and pulse_gen_1.sim/sim_1/behav/xsim/simulate.log

--- pulse_gen_1.srcs/sim_1/new/pulse_gen1_tb.v
+++ pulse_gen_1.srcs/sim_1/new/pulse_gen1_tb.v
@@ -2,8 +2,59 @@
 
 module tpulse_gen1_tb;
 reg clk =0;
-wire led;
-pulse_gen1 uut(clk, led) ;
+reg reset=1;
 
-always #5 clk = ~clk;
+wire valve_1;
+wire valve_2;
+wire discharge_1;
+wire ms_shield_1;
+wire ms_shield_2;
+wire ms_repeller_1;
+wire ms_repeller_2;
+wire ms_extractor_1;
+wire ms_extractor_2;
+wire ms_lens_1;
+wire ms_lens_2;
+wire vmi_ion_repeller_1;
+wire vmi_ion_repeller_2;
+wire vmi_ion_extractor_1;
+wire vmi_ion_extractor_2;
+wire vmi_ion_lens_1;
+wire vmi_ion_lens_2;
+wire mcp_ion;
+wire camera_ion;
+wire mass_gating;
+
+
+pulse_gen1 uut(clk,
+    reset,
+    valve_1,
+    valve_2,
+    discharge_1,
+    ms_shield_1,
+    ms_shield_2,
+    ms_repeller_1,
+    ms_repeller_2,
+    ms_extractor_1,
+    ms_extractor_2,
+    ms_lens_1,
+    ms_lens_2,
+    vmi_ion_repeller_1,
+    vmi_ion_repeller_2,
+    vmi_ion_extractor_1,
+    vmi_ion_extractor_2,
+    vmi_ion_lens_1,
+    vmi_ion_lens_2,
+    mcp_ion,
+    camera_ion,
+    mass_gating) ;
+
+always #0.5 clk = ~clk;
+
+initial begin
+   reset = 1'b1;
+   #100 reset = 1'b0; // Apply reset pulse
+   #100 reset = 1'b1;
+end
+
 endmodule
--- pulse_gen_1.srcs/sources_1/new/pulse_gen1.v
+++ pulse_gen_1.srcs/sources_1/new/pulse_gen1.v
@@ -2,16 +2,93 @@
 
 module pulse_gen1 (
     input clk,
-    output led
+    input reset,
+    
+    output valve_1,
+    output valve_2,
+    output discharge_1,
+    output ms_shield_1,
+    output ms_shield_2,
+    output ms_repeller_1,
+    output ms_repeller_2,
+    output ms_extractor_1,
+    output ms_extractor_2,
+    output ms_lens_1,
+    output ms_lens_2,
+    output vmi_ion_repeller_1,
+    output vmi_ion_repeller_2,
+    output vmi_ion_extractor_1,
+    output vmi_ion_extractor_2,
+    output vmi_ion_lens_1,
+    output vmi_ion_lens_2,
+    output mcp_ion,
+    output camera_ion,
+    output mass_gating
     );
     
 // create a simple counter
-reg [7: 0] counter =0;
-    
-always@ (posedge clk) begin
-    if (counter < 100) counter <= counter +1;   // count until 100
-    else counter <= 0; // reset counter
+reg [32: 0] counter =0;
+reg [1:0] res=0;
+
+ // Timing parameters
+ parameter CYCLE_TIME = 1e8; // 100 ms (can be adjusted)
+ parameter PULSE_WIDTH = 250;  // 250 ns (can be adjusted)
+ parameter DELAY1 = 10000;    // 10 us (can be adjusted)
+ parameter DELAY2 = 5000;    // 5 us (can be adjusted)
+ parameter MASS_GATING_WIDTH = 5; // 5 ns (can be adjusted)
+
+   
+always@ (posedge clk or negedge reset) begin
+    if(!reset) begin
+        counter<=0;
+        res<=1;
+     end else if(res==1) begin
+         if (counter < CYCLE_TIME) counter <= counter +1;   // count until 100
+        else counter <= 0; // reset counter
+    end
 end
-// create 20S duty cycle
-assign led = (counter <20) ? 1:0; // assign Zed to 1 if counter value is less
+//PULSES
+assign valve_1 = (counter > 0 && counter <= PULSE_WIDTH ) ? 1:0;
+
+assign valve_2 = (counter > DELAY1 && counter <= DELAY1 + PULSE_WIDTH ) ? 1:0; 
+
+assign discharge_1 = (counter > DELAY1 && counter <= DELAY1 + PULSE_WIDTH ) ? 1:0; 
+
+assign ms_shield_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign ms_shield_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign ms_repeller_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign ms_repeller_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign ms_extractor_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign ms_extractor_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign ms_lens_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign ms_lens_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign vmi_ion_repeller_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign vmi_ion_repeller_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign vmi_ion_extractor_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign vmi_ion_extractor_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign vmi_ion_lens_1 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign vmi_ion_lens_2 = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 0:1; 
+
+assign mcp_ion = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign camera_ion = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + PULSE_WIDTH) ? 1:0; 
+
+assign mass_gating = (counter > DELAY1 + DELAY2 && counter <= DELAY1 + DELAY2 + MASS_GATING_WIDTH) ? 1:0; 
+
+
+
+
 endmodule
